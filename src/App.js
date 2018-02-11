4a3e95360e59cba564d4614bf49c12b92b4596ad
Tamas Kapitany <kapitanytms@gmail.com>
2018-02-11 16:53:14 +0100

    Some css added to App component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import CardList from './CardList';
 import { robots } from './robots';
 import SearchBox from './SearchBox';
+import './App.css'
 const App = () => {
 	return (
 		<div className='tc'>
-			<h1>RoboFriends</h1>
+			<h1 className='f1'>RoboFriends</h1>
 			<SearchBox />
 			<CardList robots={robots} />
 		</div>
